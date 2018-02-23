593d95e419d6ae1218bd9bf39771426a08ac7325
ajchandrawat <ajchandrawat@gmail.com>
2018-02-23 16:57:16 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,6 +1,23 @@
+#Assuming the following cipher: 1=A, 2=B, 3=C,...,26=Z,27=AA, 28=AB, … write a python function which takes two integers m and n (1 < m < n < 676) and prints the character equivalents of each in a new line. For eg. if the input was m=25 and n=29, it will output:
+#Y
+#Z
+#AA
+#AB
+#AC
+
+
+
+
 def printSeries(m,n):
-	for i in range(m-1,n):
-		if i>=26:
-				print chr(int(i/26)-1 + 65), 
-		print chr(i%26 + 65)
-printSeries(25,29)
+	    for i in range(m,(n+1)):
+	        div=i
+	        string=""
+	        while div>0:
+	            mod=(div-1)%26
+	            string=chr(65+mod)+string
+	            div=int((div-mod)/26)
+	        print string
+	    return None
+	    
+m, n = [int(x) for x in raw_input("Enter values of m & n here: ").split()]
+printSeries(m,n)
