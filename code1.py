28498cae117ac7d81149639ebb5b3b123006a42b
ajchandrawat <ajchandrawat@gmail.com>
2018-02-23 16:56:16 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,3 +1,5 @@
+#Write a function that accepts a list of strings from stdin, and sorts it based on length of those strings. The output is the sorted list of strings.
+
 l=[]
 print "enter the size of the list:"
 N = int(input())
